961cc1f6c1beca751ed124bbfe0c83ca023e0c73
Lionel Déglise <lionel.deglise@e-cosi.com>
2017-07-14 11:34:33 +0200

    Oups encore un commt en 10.0

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 def hello():
-    print 'Bonjour tout le monde en branche 10.0'
+    print 'Bonjour tout le monde en branche 10.0 !!! Oups !!!'
     
 if __name__ == '__main__':
     hello()
